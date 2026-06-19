f3e5ca4ae1123bcfc3947212ccf3000cb434d055
Ian Wilson <ian@zen-cart.com>
2026-06-19 16:33:14 +0100

    updating doc examples

--- catalog/includes/classes/ZenAiAssistDocSourceRegistry.php
+++ catalog/includes/classes/ZenAiAssistDocSourceRegistry.php
@@ -10,17 +10,43 @@ class ZenAiAssistDocSourceRegistry
                 'tags' => ['developer-docs'],
             ],
             [
-                'url' => 'https://docs.zen-cart.com/dev/plugins/',
-                'tags' => ['plugins'],
-            ],
-            [
                 'url' => 'https://docs.zen-cart.com/dev/plugins/encapsulated/',
                 'tags' => ['plugins', 'encapsulated-plugins'],
                 'required' => false,
             ],
             [
+                'url' => 'https://docs.zen-cart.com/dev/plugins/encapsulated/directory_structure/',
+                'tags' => ['plugins', 'encapsulated-plugins', 'directory-structure'],
+                'required' => false,
+            ],
+            [
                 'url' => 'https://docs.zen-cart.com/dev/plugins/encapsulated/manifests/',
-                'tags' => ['plugins', 'manifest'],
+                'tags' => ['plugins', 'encapsulated-plugins', 'manifest'],
+                'required' => false,
+            ],
+            [
+                'url' => 'https://docs.zen-cart.com/dev/plugins/encapsulated/plugin_language_files/',
+                'tags' => ['plugins', 'encapsulated-plugins', 'language-files'],
+                'required' => false,
+            ],
+            [
+                'url' => 'https://docs.zen-cart.com/dev/plugins/encapsulated/installer_classes/',
+                'tags' => ['plugins', 'encapsulated-plugins', 'installer-classes'],
+                'required' => false,
+            ],
+            [
+                'url' => 'https://docs.zen-cart.com/dev/plugins/encapsulated/class_autoloading/',
+                'tags' => ['plugins', 'encapsulated-plugins', 'class-autoloading'],
+                'required' => false,
+            ],
+            [
+                'url' => 'https://docs.zen-cart.com/dev/plugins/encapsulated/sql_installation/',
+                'tags' => ['plugins', 'encapsulated-plugins', 'sql-installation'],
+                'required' => false,
+            ],
+            [
+                'url' => 'https://docs.zen-cart.com/dev/plugins/encapsulated/converting/',
+                'tags' => ['plugins', 'encapsulated-plugins', 'converting'],
                 'required' => false,
             ],
             [
